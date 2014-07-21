c91fe93c891b91609f7ac31a5870405326cc3aff
Will <willkandrade@gmail.com>
2014-07-21 09:12:38 -0400

    Create the public folder and the initial search API

---
+++ public/js/partypage.js
@@ -0,0 +1,9 @@
+function searchMe() {
+    var spotifyApi = new SpotifyWebApi();
+    spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+        .then(function(data) {
+            console.log('Artist albums', data);
+        }, function(err) {
+            console.error(err);
+        });
+}
