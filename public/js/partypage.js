86c4a2c36f84097a9776c57f0f9f7c58e6706311
shnider42 <holtsniderc@gmail.com>
2014-07-29 17:56:19 -0400

    Party Page Updates

--- public/js/partypage.js
+++ public/js/partypage.js
@@ -1,14 +1,9 @@
-function searchMe1()
-{
-	var textBox1 = document.getElementById("searchbox1").value;
-	confirm(textBox1);
-};
-
-
 function searchMe() {
     var spotifyApi = new SpotifyWebApi();
     spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
         .then(function(data) {
+        	alert(data);
+        	document.getElementById("searchbox1").value(data);
             console.log('Artist albums', data);
         }, function(err) {
             console.error(err);
