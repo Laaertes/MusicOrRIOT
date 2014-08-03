d2760deb507951a164627305ce2e14088dcd24c2
Tim Deignan <timdeignan36@gmail.com>
2014-08-03 16:04:22 -0400

    create form page css cleanup

--- templates/create_form.php
+++ templates/create_form.php
@@ -1,11 +1,14 @@
-<div class="container">
-<div class="centered"
-		<form>
-		<div class="search-bar"><input type="text" placeholder="Party Name"></div>
-        <a class="btn btn-primary btn-lg" role="button" href="partypage.php">Create</a>
-		</form>
-		<br />
+<div class="buttons">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="find-party">
+                    <div class="centered">
+                        <div class="search-bar"><input type="text" class="form-control" placeholder="Party Name"></div>
+                        <a class="btn btn-primary btn-lg" role="button" href="partypage.php">Create</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <br />
 </div>
-
