849be2c8a2836400134381f4da7a904d79eaeb72
Will <willkandrade@gmail.com>
2014-08-05 17:34:05 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 661d551efb4a2a79184b5c2e23dd95bd69c190f8, reversing
    changes made to 04823ff53c424fc49d2abf0988e5ac19b7e62686.

--- public/findparty.php
+++ public/findparty.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     // configuration
     require("../src/config.php");
@@ -24,31 +23,4 @@
              apologize("That party does not exist.");
         }
     }
-=======
-<?php
-    // configuration
-    require("../src/config.php");
-    
-    // if form was submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
-        // validate submission
-        if (empty($_POST["party"]))
-        {
-            apologize("You must provide a party name.");
-        }
-        
-        // check of party name already exists
-        $result = query("SELECT * FROM Party WHERE name = ?", $_POST["party"]);
-        if ($result)
-        {
-            // redirect to queue
-            redirect("/partypage.php");
-        }
-        else
-        {
-             apologize("That party does not exist.");
-        }
-    }
->>>>>>> origin/master
 ?>
